d4c9e31509926b984f5fc36a7cef488a51d6e398
Martin Stanev <lvl14egiant@gmail.com>
2026-04-14 08:20:15 +0200

    actually maui app

--- POS/Maui-App/MauiApp/MauiApp/ViewModels/MainViewModel.cs
+++ POS/Maui-App/MauiApp/MauiApp/ViewModels/MainViewModel.cs
@@ -1,6 +1,38 @@
-namespace MauiApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
 
-public class MainViewModel
+
+namespace FirstMauiMVVMApp.ViewModels;
+
+public class MainViewModel : BindableObject
 {
-    
+    private int _count;
+
+    public int Count
+    {
+        get => _count;
+        set
+        {
+            _count = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(ButtonText));
+        }
+    }
+
+    public string ButtonText =>
+        Count == 0 ? "Click me" : $"Clicked {Count} times";
+
+    public ICommand ClickCommand { get; }
+
+    public MainViewModel()
+    {
+        ClickCommand = new Command(OnClicked);
+    }
+
+    private void OnClicked()
+    {
+        Count++;
+    }
 }
